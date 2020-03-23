1d51198c307437d66c58cefdc436c0fb0d6cf9c8
Gabriel Ciulei <ciulei.gabriel@gmail.com>
2020-03-23 18:23:26 +0100

    storage contract

--- contracts/EtherlessSmart.sol
+++ contracts/EtherlessSmart.sol
@@ -1,12 +1,25 @@
 pragma solidity >=0.5.0 <0.7.0;
 pragma experimental ABIEncoderV2;
 
+import './FunctionsStorage.sol';
+
 contract EtherlessSmart {
+
+    FunctionsStorage private fnStorage;
+
+    /* EVENTS */
+    event RemoteExec(string _name, string _parameters, string _identifier);
+    event RemoteResponse(string _response, string _identifier);
+
+    constructor () public {
+        fnStorage = new FunctionsStorage();
+    }
+
     function listFunctions()
         public
-        pure
-        returns (string memory functionNames)
+        view
+        returns (string[] memory functionNames)
     {
-        return "dsf";
+        return fnStorage.getFunctions();
     }
 }
---
+++ contracts/FunctionsStorage.sol
@@ -0,0 +1,45 @@
+pragma solidity >=0.5.0 <0.7.0;
+pragma experimental ABIEncoderV2;
+
+import { Utils } from '../libraries/utils.sol';
+
+contract FunctionsStorage {
+    mapping(string => Utils.Function) private deployedFunctions; // <nome funzione> -> <Function>
+    string[] public availableFunctionNames;
+    mapping(address => string[]) private userFunctionNames;
+
+    function getFunctions()
+        public
+        view
+        returns (string[] memory functionNames)
+    {
+        return availableFunctionNames;
+    }
+
+    // check if functino with given name exists
+    function existsFunction(string memory named)
+        private
+        view
+        returns (bool)
+    {
+        bool found = false;
+        for (uint256 i = 0; i < availableFunctionNames.length; i++) {
+            found = (Utils.compareStrings(availableFunctionNames[i], named));
+            if (found) break;
+        }
+        return found;
+    }
+
+    function storeFunction(Utils.Function memory fn)
+        private
+    {
+        // check if function already exists before adding it to the availableFunctionNames
+        if (existsFunction(fn.name)) {
+            revert("Function already exits and cannot be created");
+        }
+        // check if another funcion is already linked to remote resource
+        deployedFunctions[fn.name] = fn;
+        availableFunctionNames.push(fn.name);
+        userFunctionNames[fn.owner].push(fn.name);
+    }
+}
